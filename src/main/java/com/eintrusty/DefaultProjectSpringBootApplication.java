bd4c4dc1f27c72423357fc730ac914289bd46203
ahrop <eintrusty@gmail.com>
2018-08-25 15:13:30 +0700

    first commit

---
+++ src/main/java/com/eintrusty/DefaultProjectSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.eintrusty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultProjectSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultProjectSpringBootApplication.class, args);
+	}
+}
